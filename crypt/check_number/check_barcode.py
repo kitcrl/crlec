075ca19dcdc3fb320743f828879cdf50266f0a3d
shinbaad <shinbaad@gmail.com>
2022-09-25 15:56:04 +0900

    a

--- crypt/check_number/check_barcode.py
+++ crypt/check_number/check_barcode.py
@@ -1,7 +1,5 @@
 print(' 바코드 검증 ');
-#barcode_n = input(' 바코드 12자리 : ');
-#barcode_n = [9,7,9,1,1,9,2,2,2,3,7,8];
-barcode_n = [2,1,0,0,0,0,0,2,0,2,2,9];
+barcode_n = input(' 바코드 12자리 : ');
 barcode = 0;
 
 for i in range(len(barcode_n)): # i :  0 ~ 11
